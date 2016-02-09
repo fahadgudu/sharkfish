5b8f9aeb9ccf67e5ecb2a1c47b71482e4c8aaa4d
Fahad Idrees <fahad@dapperapps.com.au>
2016-02-10 03:38:32 +0500

    added timestamp in thread

--- thread.rb
+++ thread.rb
@@ -1,5 +1,6 @@
 # coding: utf-8
 require 'thread'
+require "benchmark"
 
 class Semaphore
   def initialize(size = 1)
@@ -34,19 +35,29 @@ end
 
 if $0 == __FILE__
   # Example:
-  sem = Semaphore.new(3)
+  sem = Semaphore.new(1)
 
-  threads = Array.new(10) do |i|
+  threads = Array.new(8) do |i|
     Thread.start do
       puts "#{i} trying to enter…"
       sem.synchronize do
-        puts "#{i} has entered!"
-        sleep 5
+        time = Benchmark.realtime do
+          puts "#{i} has entered!"
+          puts Thread.list.select {|thread| thread.status == "run"}.count
+          sleep 1
+        end
+        puts "#{i} process took to #{time} execute"
       end
-      sleep 0.5
+      sleep 1
       puts "#{i} has left!"
     end
   end
-
   threads.map(&:join)
+end
+
+def time_method(method, *args)
+  beginning_time = Time.now
+  self.send(method, args)
+  end_time = Time.now
+  puts "Time elapsed #{(end_time - beginning_time)*1000} milliseconds"
 end
