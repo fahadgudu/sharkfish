fb58572001ea37dcd1cf72d34a4d8339eb61d08c
Fahad Idrees <fahad@dapperapps.com.au>
2016-02-11 00:56:04 +0500

    Sample

--- animal.rb
+++ animal.rb
@@ -1,5 +1,16 @@
 class Seat
 	enum seat_type: [:empty, :sit]
+  def draw(s="Empty")
+    size = 5
+    inner_size = size - 2
+    print ('*' * size)
+    (1..inner_size).each do |i|
+      print ('*' + ' ' * inner_size + '*')
+      print ('*' * size)
+      print (s)
+    end
+  end
+
 end
 
 class Table
@@ -10,27 +21,31 @@ end
 
 class Animal
   def shark_fish_sync_init
-  	
+
   end
   def shark_fish_sync_cleanup
-  	
-  end
-end  
-  
-class Fish < Animal  
-  def fish_before_eat
-  	
-  end
-  def fish_after_eat
-  	
+
   end
 end
 
-class Shark < Animal  
-  def shark_before_eat
-  	
+class Fish < Animal
+  def fish_before_eat
+
   end
-  def shark_after_eat
-  	
+  def fish_after_eat
+
   end
 end
+
+class Shark < Animal
+  def shark_before_eat
+
+  end
+  def shark_after_eat
+
+  end
+end
+
+s = Seat.new
+s.draw("FISh")
+seat.sit!
